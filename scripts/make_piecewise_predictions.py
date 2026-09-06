23977f653710a7d3a5ab0d6499442d84aeb0b570
Joseph Hellerstein <jlheller@uw.edu>
2026-09-06 12:00:39 -0700

    updated max_changepoints

--- scripts/make_piecewise_predictions.py
+++ scripts/make_piecewise_predictions.py
@@ -19,7 +19,7 @@ EXCLUDED_MODELS: List[str] = [
     "BIOMD0000000339",
 ]
 MAX_CHANGPOINTS = [0, 1, 5, 10, 12, 15, 17, 18, 19, 20]  # Maximum number of change points to consider in the piecewise model.
-MAX_CHANGPOINTS = [0, 50, 80]
+MAX_CHANGPOINTS = [0, 1, 10, 50, 80]
 MAX_FRACTIONAL_REDUCTION = 0.01  # Maximum fractional reduction in the sum of squared errors required to accept a new change point.
 COEFFICIENT_THRESHOLD = 0.001  # Threshold for coefficient magnitude to consider a species as linear.
 
